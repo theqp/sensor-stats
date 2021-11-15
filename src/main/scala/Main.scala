a50b3b1b91e3eafb24066c70c2ed5190c27f7a67
Koppany Papp <hhpkoop@gmail.com>
2021-11-15 10:13:05 +0100

    get args and handle illegal input

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,26 @@
 import cats.effect.{IO, IOApp}
-object Main extends IOApp.Simple:
-  val run = IO.println("hello world")
+import cats.effect.kernel.Resource
+import cats.effect.ExitCode
+import cats.effect.std.Console
+
+sealed abstract class SensorStatError(message: String)
+    extends Exception(message)
+final case class IllegalArgs(args: List[String])
+    extends SensorStatError(
+      s"Illegal args provided '${args.mkString(" ")}', expected a single arguement which is a path."
+    )
+
+object Main extends IOApp:
+  override def run(args: List[String]): IO[ExitCode] =
+    (args match
+      case path :: Nil => IO.pure(path)
+      case args        => IO.raiseError(IllegalArgs(args))
+    )
+      .as(ExitCode.Success)
+      .handleErrorWith {
+        case e: SensorStatError =>
+          Console[IO]
+            .errorln(s"Error: ${e.getMessage}")
+            .as(ExitCode.Error)
+        case e => IO.raiseError(e)
+      }
