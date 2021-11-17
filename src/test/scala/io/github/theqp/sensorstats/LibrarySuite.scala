5dbf8d2df8441170d145df20dde28a290e09cdae
Koppany Papp <hhpkoop@gmail.com>
2021-11-17 13:21:44 +0100

    test: can read csvs from lines

--- src/test/scala/io/github/theqp/sensorstats/LibrarySuite.scala
+++ src/test/scala/io/github/theqp/sensorstats/LibrarySuite.scala
@@ -4,6 +4,9 @@ import cats.effect.{IO, SyncIO}
 import munit.CatsEffectSuite
 import fs2.io.file.Path
 import scala.collection.immutable.TreeMap
+import cats.effect.kernel.Resource.Pure
+import fs2.io.file.Files
+import fs2.Stream
 
 class LibrarySuite extends CatsEffectSuite:
   test("can read csvs from the provided path") {
@@ -26,6 +29,28 @@ class LibrarySuite extends CatsEffectSuite:
               measurementSum = 246
             ),
             "s3" -> SensorStat.OnlyFailed
+          )
+        )
+      )
+    )
+  }
+  test("can read csvs from lines") {
+    reportFromFileLines[IO](
+      Stream
+        .emit(Stream("sensor-id,humidity", "s1,10"))
+        .repeatN(2)
+    ).assertEquals(
+      Report(
+        files = 2,
+        fileReport = FileReport(
+          failedMeasurements = 0,
+          sensorStats = TreeMap(
+            "s1" -> SensorStat.Processed(
+              min = 10,
+              max = 10,
+              measurementCount = 2,
+              measurementSum = 20
+            )
           )
         )
       )
